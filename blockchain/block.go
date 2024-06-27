7e14b38bc88613bc2adbf533d72e34bdb501bdf6
kevalsabhani <kevalsabhani@gmail.com>
2024-06-27 09:13:35 +0530

    Implement proof of work

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,25 +1,18 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 type Block struct {
 	Hash     []byte
 	PrevHash []byte
 	Data     []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+	Nonce    int
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, prevHash, []byte(data)}
-	block.DeriveHash()
+	block := &Block{[]byte{}, prevHash, []byte(data), 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+	block.Nonce = nonce
+	block.Hash = hash
 	return block
 }
 
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/kevalsabhani/go-blockchain/blockchain"
 )
@@ -17,6 +18,9 @@ func main() {
 		fmt.Printf("Previous hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Printf("**********************************************************\n")
 	}
 }
