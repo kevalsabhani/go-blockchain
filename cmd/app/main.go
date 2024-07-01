fbe8a1be1fa24ef52093e79f6bc45a438bfdc97e
Keval Sabhani <60646434+kevalsabhani@users.noreply.github.com>
2024-07-01 18:54:08 +0530

    create cli

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,9 +3,47 @@ package main
 import (
 	"fmt"
 	"strconv"
-
+	"runtime"
 	"github.com/kevalsabhani/go-blockchain/blockchain"
 )
+
+type CommandLine struct {
+	blockchain *blockchain.Blockchain
+}
+
+func (cli *CommandLine) printUsage() {
+	fmt.Println("""
+		Usage:
+		add -block BLOCK_DATA - Add a block to the chain
+		print - Prints the blocks in the chain
+	""")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.blockchain.AddBlock(data)
+	fmt.Println("Block Added!")
+}
+
+func (cli *CommandLine) printChain() {
+	iter := cli.blockchain.Iterator()
+
+	for {
+		block := iter.Next()
+		fmt.Printf("Prev. Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Printf("**********************************************************\n")
+	}
+}
 
 func main() {
 	chain := blockchain.InitBlockchain()
